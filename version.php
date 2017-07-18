dead675a24d6b82c629302ea26a42802c0477fa6
valery <valery.fremaux@gmail.com>
2017-07-18 14:43:50 +0200

    Automated sync from SVN

--- version.php
+++ version.php
@@ -27,10 +27,10 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->version   = 2016011101; // The current plugin version (Date: YYYYMMDDXX).
-$plugin->requires  = 2016051900; // Requires this Moodle version.
+$plugin->requires  = 2016112900; // Requires this Moodle version.
 $plugin->component = 'tool_cohortfromcourse'; // Full name of the plugin (used for diagnostics).
 $plugin->maturity = MATURITY_BETA;
-$plugin->release = '3.1.0 (Build 2016011100)';
+$plugin->release = '3.2.0 (Build 2016011100)';
 
 // Non moodle Attributes.
-$plugin->codeincrement = '3.1.0001';
+$plugin->codeincrement = '3.2.0001';
