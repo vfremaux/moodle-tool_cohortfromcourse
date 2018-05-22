76d71f30fbf74c40f56f9f09146c2c3b2792ff02
valery <valery.fremaux@gmail.com>
2018-05-22 21:09:48 +0200

    make M35 code repos

--- version.php
+++ version.php
@@ -27,10 +27,10 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->version   = 2016011101; // The current plugin version (Date: YYYYMMDDXX).
-$plugin->requires  = 2017110800; // Requires this Moodle version.
+$plugin->requires  = 2018042700; // Requires this Moodle version.
 $plugin->component = 'tool_cohortfromcourse'; // Full name of the plugin (used for diagnostics).
 $plugin->maturity = MATURITY_BETA;
-$plugin->release = '3.4.0 (Build 2016011100)';
+$plugin->release = '3.5.0 (Build 2016011100)';
 
 // Non moodle Attributes.
-$plugin->codeincrement = '3.4.0001';
+$plugin->codeincrement = '3.5.0001';
